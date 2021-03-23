f7d13a80676f1ba1fccf152f04de887745b383cb
Sahaj-Devulapally <76732777+Sahaj-Devulapally@users.noreply.github.com>
2021-03-23 16:52:17 +0530

    Add files via upload

--- circle.js
+++ circle.js
@@ -8,16 +8,17 @@ density:1.2
 }
 
 this.body=Bodies.circle(x,y,radius,options)
-World.add(world,this.body);
 
 this.image=loadImage("paper.png")
+World.add(world,this.body);
+
 
 
 }
 
 display(){
-ellipseMode()
-ellipse(this.body.position.x,this.body.position.y,40)
+    imageMode(CENTER)
+image(this.image,this.body.position.x,this.body.position.y,40,40)
 
 
 
